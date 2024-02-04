b73c8b5f778259528aabe911db94d5d797bbc218
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2024-02-04 21:25:26 +0000

    ayse

--- src/test/java/D6/D6Konutekrari.java
+++ src/test/java/D6/D6Konutekrari.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class D6Konutekrari {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
-        System.out.println("omer yazdi");
+        System.out.println("ayse yazdi");
         String Dayname;
         System.out.println("dayname giriniz");
         Dayname= scan.nextLine();
