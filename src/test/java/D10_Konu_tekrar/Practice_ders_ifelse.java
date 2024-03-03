c338b3bacbd6b0ab71ba71677294a0c7e186c787
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2024-03-03 14:22:43 +0000

    Tkrar8 sout silindi

--- src/test/java/D10_Konu_tekrar/Practice_ders_ifelse.java
+++ src/test/java/D10_Konu_tekrar/Practice_ders_ifelse.java
@@ -10,7 +10,7 @@ public class Practice_ders_ifelse {
 
         System.out.println("ikinci sayiyi yazdiriniz");
         int sayi2 = scan.nextInt();
-
+        System.out.println("ayse local");
         if (sayi1 > 0 && sayi2 > 0) {
             System.out.println(sayi1 + sayi2);
         } else if (sayi1 < 0 && sayi2 < 0) {
